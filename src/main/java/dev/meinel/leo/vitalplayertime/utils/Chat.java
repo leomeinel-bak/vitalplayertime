63b3937ef04182985449838f1ca3de6300e83443
Leopold Johannes Meinel <leo@meinel.dev>
2023-05-21 02:26:01 +0200

    Add FIXME for deprecated org.bukkit.ChatColor

--- src/main/java/dev/meinel/leo/vitalplayertime/utils/Chat.java
+++ src/main/java/dev/meinel/leo/vitalplayertime/utils/Chat.java
@@ -59,6 +59,7 @@ public class Chat {
         return messages;
     }
 
+    // FIXME: Replace deprecated org.bukkit.ChatColor with alternative
     public static String replaceColors(@NotNull String string) {
         return ChatColor.translateAlternateColorCodes('&', string);
     }
